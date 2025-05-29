acb3aa6dd6d3a3c7d8791384c8b7ceb1b9f9df31
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2025-05-28 21:38:30 -0300

    start java project

---
+++ src/test/java/com/fiap/Java_GlobalSolution/JavaGlobalSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.Java_GlobalSolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGlobalSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
