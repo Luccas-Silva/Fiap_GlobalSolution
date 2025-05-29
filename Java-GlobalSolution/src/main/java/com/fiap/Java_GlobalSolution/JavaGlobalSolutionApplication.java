cfe53d7f65d6242dc27af0d0e8efa7544c7507ca
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2025-05-28 21:10:42 -0300

    Start Java Project

---
+++ Java-GlobalSolution/src/main/java/com/fiap/Java_GlobalSolution/JavaGlobalSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.Java_GlobalSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGlobalSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGlobalSolutionApplication.class, args);
+	}
+
+}
