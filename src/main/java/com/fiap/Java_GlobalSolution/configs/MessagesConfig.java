11e716889e2d3f5320c88cb70ae27c3e1c3201ec
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2025-06-01 20:03:09 -0300

    nav Messages

--- src/main/java/com/fiap/Java_GlobalSolution/configs/MessagesConfig.java
+++ src/main/java/com/fiap/Java_GlobalSolution/configs/MessagesConfig.java
@@ -1,7 +1,35 @@
 package com.fiap.Java_GlobalSolution.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Locale;
 
 @Configuration
-public class MessagesConfig {
+public class MessagesConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("pt", "BR"));
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
 }
