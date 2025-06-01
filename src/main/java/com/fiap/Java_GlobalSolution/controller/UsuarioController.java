b4f52c1f8c6714c84ec9b2798fcba392e13f3e1f
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2025-05-31 22:05:04 -0300

    create database

--- src/main/java/com/fiap/Java_GlobalSolution/controller/UsuarioController.java
+++ src/main/java/com/fiap/Java_GlobalSolution/controller/UsuarioController.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/usuario")
 @RequiredArgsConstructor
 @Validated
 public class UsuarioController {
+
 }
